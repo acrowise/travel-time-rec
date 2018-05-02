20bc73c0060697df9b93a09a3e881aab6a1012fe
Kammy Deng <32975271+kammybdeng@users.noreply.github.com>
2018-05-01 21:57:20 -0700

    update filtering.py for new model

--- src/filtering.py
+++ src/filtering.py
@@ -1,11 +1,13 @@
 import pandas as pd
 
+# general filtering
 def filter_user(user_df):
     null_age = user_df.ageRange.isnull()
     null_gender = user_df.gender.isnull()
     null_style = user_df.travelStyle.isnull()
+    one_thou_pt = (user_df.totalPoints > 1000)
 
-    user_filtered = user_df[user_df.totalPoints > 1000][~null_age][~null_gender][~null_style]
+    user_filtered = user_df[one_thou_pt][~null_age][~null_gender][~null_style]
 
     user_filtered = user_filtered[['username', 'ageRange', 'gender', 'travelStyle']]
     return user_filtered
@@ -14,7 +16,7 @@ def filter_user(user_df):
 def filter_review(review_df):
 
     attraction_only = review_df.type == 'Attractions'
-    filtered_review_df = review_df[['id', 'username', 'type', 'text', 'rating', 'taObjectCity']]
+    filtered_review_df = review_df[['id', 'username', 'type', 'title', 'text', 'rating', 'taObjectCity']]
     filtered_review_df = filtered_review_df[attraction_only]
 
     return filtered_review_df
@@ -43,8 +45,111 @@ def filter_final(merged_df, popular_city_list):
     return final_df
 
 
-def prep_als_df(final_df):
 
-    asl_temp_df = final_df[['username', 'taObjectCity', 'rating']]
+# for user feature matrix
 
-    return asl_temp_df
+
+def user_feature_filter(final_df):
+
+    feature_temp = final_df[['username', 'ageRange', 'gender', 'travelStyle']]
+    feature_temp = feature_temp.drop_duplicates()
+
+    return feature_temp
+
+def travel_style(feature_temp):
+
+    style_lst = [item.split(', ') for item in feature_temp.travelStyle]
+    style_serie = pd.Series(style_lst)
+
+    feature_temp['new_travel'] = style_serie.values
+
+    return feature_temp
+
+def travel_matrix(feature_temp):
+
+
+    style_matrix = feature_temp['new_travel'].apply(pd.Series)
+    style_df = pd.get_dummies(style_matrix.apply(pd.Series). \
+                  stack()).sum(level=0). \
+                  rename(columns = lambda x : x)
+
+    return style_df
+
+def age_gender_dummie(feature_temp):
+
+    feature_temp = pd.get_dummies(feature_temp, \
+                                  columns = ['ageRange', 'gender'])
+
+    return feature_temp
+
+def combine_all_dummies(left, right):
+
+    feature_temp = left.join(right)
+    feature_final = feature_temp.drop(['travelStyle', \
+                                       'new_travel', \
+                                       'gender_male', \
+                                       '60+ Traveler', \
+                                       'username'], axis =1)
+
+    feature_final.reset_index(drop=True, inplace=True)
+
+
+    return feature_final
+
+
+# prep for clustering
+
+
+def cluster_prep_filter(final_df):
+
+    cluster_input_df = final_df.copy()
+    cluster_input_df = cluster_input_df[['title', 'text', 'taObjectCity']]
+
+    return cluster_input_df
+
+
+def grouping_city_title(cluster_input_df):
+    df_title_comb = cluster_input_df.groupby(['taObjectCity']). \
+                            apply(lambda x: ' '. \
+                            join(x.title)). \
+                            reset_index()
+
+    return df_title_comb
+
+
+
+def grouping_city_text(cluster_input_df):
+    df_text_comb = cluster_input_df.groupby(['taObjectCity']). \
+                                    apply(lambda x: ' '. \
+                                    join(x.text)). \
+                                    reset_index()
+    return df_text_comb
+
+
+def merging_content(left, right):
+
+    cluster_input_df = left.merge(right, on= 'taObjectCity')
+    cluster_input_df.columns = ['taObjectCity','title','text']
+    cluster_input_df.set_index(['taObjectCity'], drop=True, inplace=True)
+
+    return cluster_input_df
+
+
+# selected cities from cluster
+
+
+
+def selected_cities_in_cluster(cluster_df, city_cluster_idx):
+
+
+    cluster2_mask = (cluster_df['cluster_k'] == city_cluster_idx)
+    up_cluster_df = cluster_df[cluster2_mask]
+    up_cluster_df.columns = ['cluster_k', 'taObjectCity']
+
+    return up_cluster_df
+
+def selected_city_df(up_cluster_df, city_df):
+
+    selected_df = pd.merge(up_cluster_df, city_df, how = 'left', on = 'taObjectCity')
+
+    return selected_df
