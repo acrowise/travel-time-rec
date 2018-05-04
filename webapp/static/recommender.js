e28be8dc4197f8e47213eeee0806dcf0e5718363
Kammy Deng <32975271+kammybdeng@users.noreply.github.com>
2018-05-04 13:25:19 -0700

    Update recommender.js

--- webapp/static/recommender.js
+++ webapp/static/recommender.js
@@ -5,6 +5,14 @@
 // creating a function in
 // funciton - no name
 
+var cities = {
+  "Salzburg": {
+    url:'https://travelpassionate.com/wp-content/uploads/2018/01/Austria-Rainbow-over-Salzburg-castle-min.jpg',
+    country: 'Austria',
+    population: 35000
+  }
+};
+
 $(document).ready(function(){
   console.log('web page is ready');
   // async and await goes together
@@ -19,6 +27,7 @@ $(document).ready(function(){
       user
     };
     console.log(data)
+
     // browser sending the message to server who is listening
     const response = await $.ajax('/inference', {
       data: JSON.stringify(data),   // turning data into string
@@ -27,7 +36,14 @@ $(document).ready(function(){
     });
 
     console.log('response', response);
-    $('#recommender').val(response.prediction);
+    console.log('response', response.predictions.pred1);
+    console.log('response', response.predictions.pred2);
+    console.log('city_dict', cities[response.predictions.pred1]);
+
+    //const url = cities[response.prediction].url;
+    // $('#city-url').attr('src', url);
+    $('#recommender1').val(response.predictions.pred1);
+    $('#recommender2').val(response.predictions.pred2);
   //
   });
 });
